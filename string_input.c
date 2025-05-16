569a252d587e99922a5de1cba7c73f249d050321
Jade <nyanminhtet2304@gmail.com>
2025-05-16 16:02:02 +0630

    Changes on string_input

--- string_input.c
+++ string_input.c
@@ -2,9 +2,9 @@
 
 int main(void)
 {
-    char name[20];
+    char name[100];
     printf("Enter your name: ");
-    scanf("%19s", name); // Limit input to 19 characters to prevent buffer overflow
+    scanf("%99s", name); // Limit input to 19 characters to prevent buffer overflow
     printf("Hello, %s!\n", name);
     return 0;
 }
